b0922b234161f8cb6d62f37a3b7ad915cf75bf7a
catalina <catalinaruedaf@hotmail.com>
2026-06-18 14:15:53 +1000

    starting styling the page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import heromobileimage from "../images/image-profile-mobile.webp"
+import herotableimage from "../images/image-profile-mobile.webp";
+import herodesktopimage from "../images/image-profile-desktop.webp";
 
-import imageIntro from "../assets/images/image-hero-mobile.webp";
-import imageTablet from "../assets/images/image-hero-tablet.webp";
-import imageDesktop from "../assets/images/image-hero-desktop.webp";
 
-import heromobileimage from "../../public/images/image-profile-mobile.webp";
-import herotabletimage from "../../public/images/image-profile-tablet.webp";
-import herodesktopimage from "../../public/images/image-profile-desktop.webp";
+import circlepattern from "../images/pattern-circle.svg";
+import rings from "../images/pattern-rings.svg";
+
 
 
 
@@ -17,47 +17,43 @@ import herodesktopimage from "../../public/images/image-profile-desktop.webp";
 export default function Hero(){
     return (
  <section className="hero__section ">
-   
-       <header className="hero__header container">
-       
-     
-        <div className="wrapper__hero-content ">
-           
-            <div className="info__hero">
-                
-                <h1 className="title__hero">Nice to meet you! I'm <span>Catalina G.</span></h1>
-                <p className="hero_text">
-                 Based in Australia and working as a freelance designer, I create digital experiences for brands and agencies around the world. 
-                 I'm passionate about creating accessible, human-centered products and digital experiences.
-                </p>
-                <Link to="/contact" className="btn__hero">Contact me</Link>
-                            
-            </div>
-
+    <header className="hero__header container">
+       <img src={circlepattern} alt="circle pattern" className="circle__pattern" />
+         <img src={rings} alt="rings pattern" className="rings__pattern" />
+    <div className="wrapper__hero-content ">
            
       
-
             <div className="image__hero">
                 <picture className="image__hero">
                         <source
                             media="(min-width: 1024px)"
                             srcSet={herodesktopimage}
                         />
-
                         <source
                             media="(min-width: 710px)"
-                            srcSet={herotabletimage}
+                            srcSet={herotableimage}
                         />
-
                         <img
                             src={heromobileimage}
-                            alt="photo of Catalina G."
+                            alt="photo of Adam Keyes.."
                         />
                         </picture>
-           
                 </div>
+
+
+                <div className="info__hero">
+                   <h1 className="title__hero">Nice to meet you! I'm <span className="highlight">Adam Keyes.</span></h1>
+                   <p className="hero_text">
+                    Based in the UK, I’m a front-end developer
+                    passionate about building accessible web apps that users love. I’ve had the privilege of working at a start-up and a huge corporation. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement.
+                  </p>
+                <Link to="/contact" className="btn__hero">Contact me</Link>
+                            
+              </div>
+
+           
          </div>
-         </header>
+ </header>
           
     
        
---
+++ src/pages/home.jsx
@@ -0,0 +1,29 @@
+import Hero from "../components/Hero";
+
+
+
+
+
+
+export default function Home () {
+    return (
+        <>
+     
+           <Hero/>
+          <main>
+           
+
+            </main>
+      
+
+     
+        
+   
+            
+           
+            
+           
+            
+        </>
+    );
+}
